34a8d264c6619e39f388a395a74c6156409af6b5
Clint Kingston <ckingston1@woolworths.com.au>
2022-04-10 21:26:33 +1000

    seperated footer css from main css

---
+++ to be deleted/index.js
@@ -0,0 +1,19 @@
+function openNav(){
+    var navHeight = parseInt(document.getElementById("navigation-bar").style.height)
+    console.log("button clicked",navHeight)
+    if(navHeight == 0 ||isNaN(navHeight)){
+    document.getElementById("navigation-bar").style.height = "240px"
+    }else{
+        document.getElementById("navigation-bar").style.height = "0px"
+    }
+}
+
+function reportResize(){
+    console.log("window is being resized")
+    document.getElementById("navigation-bar").style.height = "0px"
+    if(window.innerWidth >900){
+       document.getElementById("navigation-bar").style.height = "36px" 
+    }
+}
+
+window.onresize = reportResize;
