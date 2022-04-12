66bb2e29ac02462649badff3bd736d2a13d262ec
Clint Kingston <ckingston1@woolworths.com.au>
2022-04-12 19:43:21 +1000

    gallery caption complete

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,10 +1,16 @@
-function openImg(event){
+function openImg(event,altText){
     console.log("image clicked src returned = ",event)
 var theImage=event;
+var caption=altText;
 console.log("isrc stored in var = ",theImage)
 document.getElementById("zoomedImage").src = theImage
 document.getElementById("zoomImg").style.display = "flex"
+ 
+console.log("image alt text = ",caption);
+document.getElementById("galleryCaption").innerHTML=caption;
 }
+
+
 
 function closeImage(){
     document.getElementById("zoomImg").style.display = "none"
